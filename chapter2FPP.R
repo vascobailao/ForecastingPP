8363c1a08f5a2e60a80071621ee40e0779828a04
vascobailao <vasco.bailao@gmail.com>
2018-09-30 23:07:38 +0100

    update

--- chapter2FPP.R
+++ chapter2FPP.R
@@ -79,10 +79,67 @@ ggsubseriesplot(h02)
 # Exercise 6
 
 help(hsales)
+autoplot(hsales)
 ggseasonplot(hsales)
-ggsubseriesplot(hsales)
 ggsubseriesplot(hsales)
 gglagplot(hsales)
 ggAcf(hsales)
 
+help(usdeaths)
+autoplot(usdeaths)
+ggseasonplot(usdeaths)
+ggsubseriesplot(usdeaths)
+gglagplot(usdeaths)
+ggAcf(usdeaths)
+
+help(bricksq)
+autoplot(bricksq)
+ggseasonplot(bricksq)
+ggsubseriesplot(bricksq)
+gglagplot(bricksq)
+ggAcf(bricksq)
+
+help(sunspotarea)
+autoplot(sunspotarea)
+ggseasonplot(sunspotarea) # Data comes in years -> not sazonal!
+ggsubseriesplot(sunspotarea) # Data comes in years -> not sazonal!
+gglagplot(sunspotarea)
+ggAcf(sunspotarea)
+
+help(gasoline)
+autoplot(gasoline)
+ggseasonplot(gasoline)
+ggsubseriesplot(gasoline)
+gglagplot(gasoline)
+ggAcf(gasoline)
+
+# Exercise 7
+
+help(arrivals)
+autoplot(arrivals)
+arrivals.japan <- arrivals[,1]
+arrivals.nz <- arrivals[,2]
+arrivals.uk <- arrivals[,3]
+arrivals.us <- arrivals[,4]
+ggseasonplot(arrivals.japan)
+ggseasonplot(arrivals.nz)
+ggseasonplot(arrivals.uk)
+ggseasonplot(arrivals.us)
+ggsubseriesplot(arrivals.japan)
+ggsubseriesplot(arrivals.nz)
+ggsubseriesplot(arrivals.uk)
+ggsubseriesplot(arrivals.us)
+
+# Exercise 8
+# 1-B ; 2-A; 3-D; 4-C
+
+# Exercise 9
+mypigs <- window(pigs, start=1990)
+autoplot(mypigs)
+ggAcf(mypigs)
+
+# Exercise 10
+ddj <- diff(dj)
+autoplot(ddj)
+ggAcf(ddj)
 
