f70a70cc1325e7af31bf9cb4fe252f3f84d07828
vascobailao <vasco.bailao@gmail.com>
2018-10-01 19:15:38 +0100

    update

--- Chapter3FPP.R
+++ Chapter3FPP.R
@@ -61,3 +61,93 @@ autoplot(eggs) +
   guides(colour=guide_legend(title="Forecast"))
 
 # Residuals diagnostic
+
+# A good forecasting method will yield residuals with the following properties:
+#   
+# 1.The residuals are uncorrelated. 
+# If there are correlations between residuals, then there is information left 
+# in the residuals which should be used in computing forecasts.
+# 2.The residuals have zero mean. If the residuals have a mean other than zero, 
+# then the forecasts are biased.
+# 3.The residuals have constant variance.
+# 4. he residuals are normally distributed.
+
+# Example: Google stock price forecast using naive algorithm
+
+autoplot(goog200) +
+  xlab("Day") + ylab("Closing Price (US$)") +
+  ggtitle("Google Stock (daily ending 6 December 2013)")
+
+res <- residuals(naive(goog200))
+autoplot(res) + xlab("Day") + ylab("") +
+  ggtitle("Residuals from naïve method")
+
+gghistogram(res) + ggtitle("Histogram of residuals")
+
+ggAcf(res) + ggtitle("ACF of residuals")
+
+checkresiduals(naive(goog200))
+
+# Evaluating forecast accuracy
+
+# extracts all data from 1995 onward.
+window(ausbeer, start=1995)
+
+# extracts the last 5 years of observations from ausbeer
+subset(ausbeer, start=length(ausbeer)-4*5)
+
+# extracts the first quarters for all years.
+subset(ausbeer, quarter = 1)
+
+# last 5 years of ausbeer
+tail(ausbeer, 4*5)
+
+# Types of errors:
+
+# 1. Mean Absolute Error (MAE): vertical distance between the prediction and the ground truth
+# over the total number of samples
+
+# Example 1
+beer2 <- window(ausbeer,start=1992,end=c(2007,4))
+beerfit1 <- meanf(beer2,h=10)
+beerfit2 <- rwf(beer2,h=10)
+beerfit3 <- snaive(beer2,h=10)
+autoplot(window(ausbeer, start=1992)) +
+  autolayer(beerfit1, series="Mean", PI=FALSE) +
+  autolayer(beerfit2, series="Naïve", PI=FALSE) +
+  autolayer(beerfit3, series="Seasonal naïve", PI=FALSE) +
+  xlab("Year") + ylab("Megalitres") +
+  ggtitle("Forecasts for quarterly beer production") +
+  guides(colour=guide_legend(title="Forecast"))
+
+beer3 <- window(ausbeer, start=2008)
+accuracy(beerfit1, beer3)
+accuracy(beerfit2, beer3)
+accuracy(beerfit3, beer3)
+
+# Example 2
+googfc1 <- meanf(goog200, h=40)
+googfc2 <- rwf(goog200, h=40)
+googfc3 <- rwf(goog200, drift=TRUE, h=40)
+autoplot(subset(goog, end = 240)) +
+  autolayer(googfc1, PI=FALSE, series="Mean") +
+  autolayer(googfc2, PI=FALSE, series="Naïve") +
+  autolayer(googfc3, PI=FALSE, series="Drift") +
+  xlab("Day") + ylab("Closing Price (US$)") +
+  ggtitle("Google stock price (daily ending 6 Dec 13)") +
+  guides(colour=guide_legend(title="Forecast"))
+
+googtest <- window(goog, start=201, end=240)
+accuracy(googfc1, googtest)
+accuracy(googfc2, googtest)
+accuracy(googfc3, googtest)
+
+# Predictions Intervals
+
+naive(goog200)
+autoplot(naive(goog200))
+
+# with bootstapping
+naive(goog200, bootstrap=TRUE)
+autoplot(naive(goog200))
+
