57fe4abe05acfd7fd09491e14a956bba49efb127
vascobailao <vasco.bailao@gmail.com>
2018-10-08 11:45:45 +0100

    update

--- Chapter5FPP.R
+++ Chapter5FPP.R
@@ -110,4 +110,140 @@ fit <- tslm(Demand ~ Temperature, data=daily20)
 checkresiduals(fit)
 forecast(fit, newdata=data.frame(Temperature=c(15,35)))
 
-Chapter4FPP.R
+elecdaily %>%
+  as.data.frame() %>%
+  ggplot(aes(x=Temperature, y=Demand)) +
+  ylab("Electricity Demand") +
+  xlab("Temperature") +
+  geom_point() +
+  geom_smooth(method="lm", se=FALSE)
+
+# Exercise 2
+data("mens400")
+autoplot(mens400)
+
+time_mens400 <- time(mens400)
+tslm_mens400 <- tslm(mens400 ~ time_mens400, 
+                     data = mens400)
+
+mens400 %>%
+  as.data.frame() %>%
+  ggplot(aes(x=time_mens400, y=mens400)) +
+  geom_point()
+
+# Show data with regression line
+autoplot(mens400) +
+  geom_abline(slope = tslm_mens400$coefficients[2],
+              intercept = tslm_mens400$coefficients[1],
+              colour = "red")
+  
+# Show data (scatter) with regression line
+autoplot(mens400) +
+  geom_point() +
+  geom_abline(slope = tslm_mens400$coefficients[2],
+            intercept = tslm_mens400$coefficients[1],
+            colour = "red")
+
+# Average decrease in times
+# Get the winning time decreasing rate
+tslm_mens400$coefficients[2]
+# The winning times have been decreasing at average rate of 0.06457 second per year.
+
+# c. Plot the residuals against the year. What does this indicate about the suitability of the fitted line?
+cbind(Time = time_mens400, 
+      Residuals = tslm_mens400$residuals) %>%
+  as.data.frame() %>%
+  ggplot(aes(x = Time, y = Residuals)) +
+  geom_point() +
+  ylab("Residuals of Regression Line(Unit:s)")
+# The residual plot shows that the regression model generally fitted the data well. I can check it using checkresiduals function, too.
+checkresiduals(tslm_mens400)
+  
+# d. Predict the winning time for the men's 400 meters final in the 2020 Olympics. 
+# Give a prediction interval for your forecasts. 
+# What assumptions have you made in these calculations?
+# I made linear model with na.action argument as na.exclude to exclude missing values.
+# And then I used the linear model in forecast function to get prediction interval.
+# Forecast function can't calculate prediction interval when there is any missing values 
+# in the data that I excluded them fitting linear model.
+
+# FIRST; fit a linear model of the data
+lm_mens400 <- lm(
+  mens400 ~ time_mens400, 
+  data = mens400,
+  na.action = na.exclude
+)
+# SECOND: make a forecast for time_mens400 = 2020.
+fc_mens400 <- forecast(
+  lm_mens400, 
+  newdata = data.frame(time_mens400 = 2020)
+)
+
+autoplot(mens400) +
+  autolayer(fc_mens400, PI = TRUE)
+
+# Get 80% and 95% prediction intervals
+fc_mens400$upper
+fc_mens400$lower
+
+# Exercise 3
+
+beer_data <- easter(ausbeer)
+autoplot(beer_data)
+time(beer_data[0])
+time(beer_data[length(time[beer_data])-1])
+
+# Exercise 5
+
+# a. We can see that there's a trend (sales increase with years)
+# We can see that the volume of sales from 92 forward increases in an exponencial way
+autoplot(fancy)
+
+# b. Explain why it is necessary to take logarithms of these data before fitting a model.
+# The size of the seasonal variations should be almost same across the whole series to be fitted well to a model. Fancy data shows that seasonal variations increased exponentially. Therefore it is necessary to take logarithms of the data.
+
+# c.
+Time <- time(fancy)
+surfing_festival <- c()
+for(i in 1:length(Time)){
+  month <- round(12*(Time[i] - floor(Time[i]))) + 1
+  year <- floor(Time[i])
+  if(year >= 1988 & month == 3){
+    surfing_festival[i] <- 1
+  } else {
+    surfing_festival[i] <- 0
+  }
+}
+
+tslm_log_fancy <- tslm(
+  BoxCox(fancy, 0) ~ trend + season + surfing_festival
+)
+
+# d.
+autoplot(tslm_log_fancy$residuals)
+# The residuals have pattern against time. 
+# It means that there is correlation between residuals and time.
+
+cbind(Residuals = tslm_log_fancy$residuals,
+      Fitted_values = tslm_log_fancy$fitted.values) %>%
+  as.data.frame() %>%
+  ggplot(aes(x = Fitted_values,
+             y = Residuals)) +
+  geom_point()
+
+# e.
+
+cbind.data.frame(
+  Month = factor(
+    month.abb[round(12*(Time - floor(Time)) + 1)],
+    labels = month.abb,
+    ordered = TRUE
+  ),
+  Residuals = tslm_log_fancy$residuals
+) %>%
+  ggplot(aes(x = Month,
+             y = Residuals)) +
+  geom_boxplot()
+
+checkresiduals(tslm_log_fancy$residuals)
+
